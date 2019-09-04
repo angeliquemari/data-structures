67dba3e69d8a0de8136e30b7a2aa1d2603506ed6
Angelique Nehmzow <angelique@nehmzow.com>
2019-09-04 11:56:23 -0700

    complete methods in Queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,24 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    var newKey = parseInt(Object.keys(storage).pop()) + 1;
+    if (isNaN(newKey)) {
+      newKey = 0;
+    }
+    storage[newKey] = value;
+    someInstance[newKey] = value;
   };
 
   someInstance.dequeue = function() {
+    var removeKey = Object.keys(storage).shift();
+    var returnItem = storage[removeKey];
+    delete storage[removeKey];
+    delete someInstance[removeKey];
+    return returnItem;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;

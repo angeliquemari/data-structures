d4d1a12b391b8f475ae29977ca6e628fd75419f1
Angelique Nehmzow <angelique@nehmzow.com>
2019-09-04 15:28:56 -0700

    almost complete psuedoclassical section

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -3,4 +3,21 @@ var Queue = function() {
   // but try not not reference your old code in writing the new style.
 };
 
+Queue.prototype.enqueue = function(value) {
+  var newKey = parseInt(Object.keys(this).pop()) + 1;
+  if (isNaN(newKey)) {
+    newKey = 0;
+  }
+  this[newKey] = value;
+};
 
+Queue.prototype.dequeue = function() {
+  var removeKey = Object.keys(this).shift();
+  var removeItem = this[removeKey];
+  delete this[removeKey];
+  return removeItem;
+};
+
+Queue.prototype.size = function() {
+  return Object.keys(this).length;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -3,4 +3,21 @@ var Stack = function() {
   // but try not not reference your old code in writing the new style.
 };
 
+Stack.prototype.push = function(value) {
+  var newKey = parseInt(Object.keys(this).pop()) + 1;
+  if (isNaN(newKey)) {
+    newKey = 0;
+  }
+  this[newKey] = value;
+};
 
+Stack.prototype.pop = function() {
+  var removeKey = Object.keys(this).pop();
+  var removeItem = this[removeKey];
+  delete this[removeKey];
+  return removeItem;
+};
+
+Stack.prototype.size = function() {
+  return Object.keys(this).length;
+};

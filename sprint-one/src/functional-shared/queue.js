12482e3c9c07e5ad63fe042bd87ae398197f2108
Angelique Nehmzow <angelique@nehmzow.com>
2019-09-04 13:25:47 -0700

    complete function shared pattern for Queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = {};
+  _.extend(newQueue, queueMethods);
+  return newQueue;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  var newKey = parseInt(Object.keys(this).slice(0, Object.keys(this).length - Object.keys(queueMethods).length).pop()) + 1;
+  if (isNaN(newKey)) {
+    newKey = 0;
+  }
+  this[newKey] = value;
+};
 
+queueMethods.dequeue = function() {
+  var removeKey = parseInt(Object.keys(this).slice(0, Object.keys(this).length - Object.keys(queueMethods).length).shift());
+  var removeItem = this[removeKey];
+  delete this[removeKey];
+  return removeItem;
+};
+
+queueMethods.size = function() {
+  return Object.keys(this).length - Object.keys(queueMethods).length;
+};

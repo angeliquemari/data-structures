ac9f0eeab3d6cda3c35ac0ddb83c509814c8bc4e
Angelique Nehmzow <angelique@nehmzow.com>
2019-09-04 17:30:11 -0700

    complete es6 pattern

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,28 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+  }
+
+  enqueue (value) {
+    var keys = Object.keys(this);
+    keys.pop();
+    var newKey = parseInt(keys.pop()) + 1;
+    if (isNaN(newKey)) {
+      newKey = 0;
+    }
+    this[newKey] = value;
+  }
+
+  dequeue () {
+    var removeKey = parseInt(Object.keys(this).shift());
+    var removeItem = this[removeKey];
+    delete this[removeKey];
+    return removeItem;
+  }
+
+  size () {
+    return Math.max(0, Object.keys(this).length - 1);
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,30 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+  }
+
+  push (value) {
+    var keys = Object.keys(this);
+    keys.pop();
+    var newKey = parseInt(keys.pop()) + 1;
+    if (isNaN(newKey)) {
+      newKey = 0;
+    }
+    this[newKey] = value;
+  }
+
+  pop () {
+    var keys = Object.keys(this);
+    keys.pop();
+    var removeKey = parseInt(keys.pop());
+    var removeItem = this[removeKey];
+    delete this[removeKey];
+    return removeItem;
+  }
+
+  size () {
+    return Math.max(0, Object.keys(this).length - 1);
   }
 
 }

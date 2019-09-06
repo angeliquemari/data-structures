ae704d4026998261005c9ef1e244c0ec3511e6c6
angeliquemari <angelique@nehmzow.com>
2019-09-05 17:56:25 -0700

    complete linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,42 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+
+    var node = Node(value);
+    var key;
+    if (list.head === null) {
+      key = 0;
+      list.head = node;
+    } else {
+      key = list.tail.next + 1;
+      list.tail.next = key;
+    }
+    list[key] = node;
+    list.tail = list[key];
   };
 
   list.removeHead = function() {
+    var oldHeadNode = list.head.value;
+    var newHeadPosition = list.head.next;
+    list.head = list[newHeadPosition];
+    return oldHeadNode;
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+    var endOfList = false;
+    while (endOfList === false) {
+      if (currentNode.value === target) {
+        return true;
+      }
+      if (currentNode.next === null) {
+        endOfList = true;
+      } else {
+        currentNode = list[currentNode.next.toString()];
+      }
+    }
+    return false;
   };
-
   return list;
 };
 
@@ -26,4 +54,7 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * addToTail: O(1) because we know where the tail is in order to add the next node.
+ * removeHead: O(1) because we know where the head is to remove the first node.
+ * contains: O(n) because in the worse case one would have to traverse the whole list
  */
